637a81f72a4680d1660558e6f653eb8c4f95a615
Dias <lord.taiko@mail.ru>
2020-05-13 17:22:05 +0600

    [ADD] prject cloud

---
+++ first/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dias\documents\django_projects_disa\weather_app\first\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ weather_app/weather/views.py
@@ -0,0 +1,69 @@
+
+
+import requests
+from django.shortcuts import render, redirect
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=5efcc72ef08fbdcd883266b64fddddea'
+
+    err_m = ''
+    message = ''
+    message_class = ''
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            exist_city_count = City.objects.filter(name=new_city).count()
+
+            if exist_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_m = 'It is not correct'
+            else:
+                err_m = 'City already exist'
+        if err_m:
+            message = err_m
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully'
+            message_class = 'is-success'
+
+
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {
+    'weather_data' : weather_data,
+    'form' : form,
+    'message': message,
+    'message_class': message_class,
+    }
+
+    return render(request, 'weather/index.html', context)
+
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
